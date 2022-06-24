1e1219035e0bf7bf6f21a4fdae6e7e4f2e400a81
MISSIC <prajjwalt9@gmail.com>
2022-06-24 09:08:58 +0545

    setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <main className='relative bg-brickred h-[100vh] w-full'>
+           <nav></nav>
+           <div className="clock-section"></div>
+           <div className="spotify-section"></div>
+    </main>
+    </>
   );
 }
 
